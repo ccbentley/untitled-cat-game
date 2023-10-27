a20cb858c3c18f564c823be318280ea35daf3048
ccbentley <136651657+ccbentley@users.noreply.github.com>
2023-10-26 20:58:14 -0400

    KB Depending On Local Scale Fix

--- Assets/Scripts/StateMachine/FadeRemoveBehaviour.cs
+++ Assets/Scripts/StateMachine/FadeRemoveBehaviour.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class FadeRemoveBehaviour : StateMachineBehaviour
 {
     public float fadeTime = 0.5f;
+    public float fadeDelay = 0.0f;
     private float timeElapsed = 0f;
+    private float fadeDelayElapsed = 0f;
     SpriteRenderer spriteRenderer;
     GameObject objToRemove;
     Color startColor;
-
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -23,15 +24,23 @@ public class FadeRemoveBehaviour : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        timeElapsed += Time.deltaTime;
-
-        float newAlpha = startColor.a * (1 - (timeElapsed / fadeTime));
-
-        spriteRenderer.color = new Color(startColor.r, startColor.g, startColor.b, newAlpha);
-
-        if(timeElapsed > fadeTime)
+        Debug.Log("test");
+        if (fadeDelay > fadeDelayElapsed)
         {
-            Destroy(objToRemove);
+            fadeDelayElapsed += Time.deltaTime;
+        }
+        else
+        {
+            timeElapsed += Time.deltaTime;
+
+            float newAlpha = startColor.a * (1 - (timeElapsed / fadeTime));
+
+            spriteRenderer.color = new Color(startColor.r, startColor.g, startColor.b, newAlpha);
+
+            if (timeElapsed > fadeTime)
+            {
+                Destroy(objToRemove);
+            }
         }
     }
 }

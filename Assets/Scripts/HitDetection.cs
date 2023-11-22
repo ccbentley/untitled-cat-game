38fb1e4fcc480097cef9d34c04b3d6c83a5df08f
ccbentley <136651657+ccbentley@users.noreply.github.com>
2023-11-22 17:50:08 -0500

    Fixed Hit Sound And Breakable Lights

--- Assets/Scripts/HitDetection.cs
+++ Assets/Scripts/HitDetection.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 
-[RequireComponent(typeof(Rigidbody2D), typeof(Damageable))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Damageable), typeof(AudioSource))]
 public class HitDetection : MonoBehaviour
 {
     Rigidbody2D rb;
     Animator animator;
     Damageable damageable;
+    AudioSource audioSource;
+    GameObject player;
+
+    [Header("Audio")]
+    public AudioClip hit1;
+    public float hit1Volume = 1;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         damageable = GetComponent<Damageable>();
+        player = GameObject.FindWithTag("Player");
+        audioSource = player.GetComponent<AudioSource>();
+
     }
 
     public void OnHit(int damage, Vector2 knockback)
     {
         rb.velocity = new Vector2(knockback.x, rb.velocity.y + knockback.y);
+        audioSource.PlayOneShot(hit1, hit1Volume);
     }
 }
--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -7,16 +7,6 @@ public class Attack : MonoBehaviour
     public int attackDamage = 10;
     public Vector2 knockback = Vector2.zero;
 
-    [Header("Audio")]
-    AudioSource audioSource;
-    public AudioClip hit1;
-    public float hit1Volume = 1;
-
-    private void Awake()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // See if it can be hit
@@ -29,9 +19,6 @@ public class Attack : MonoBehaviour
 
             // Hit the target
             bool gotHit = damageable.Hit(attackDamage, deliveredKnockback);
-
-            audioSource.PlayOneShot(hit1, hit1Volume);
-
         }
     }
 }
